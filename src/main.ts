7a8fcdfdc0d80c98ca3aee6d6a8bf6f6c1d7762a
nitesh sinwar <w3docs_email@example.com>
2023-03-24 23:22:51 +0530

    finally done

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
   /* process.env.PORT)*/
 }
 bootstrap();
